cf813c4b853b900684d2cff2e55629810f6f135b
Adam Keefer <adamjkeefer@gmail.com>
2023-11-20 16:38:02 -0500

    Update config.py

--- pyspider/src/config.py
+++ pyspider/src/config.py
@@ -12,20 +12,20 @@ class PrioritizerQueueConfig:
 
 prioritizer_queue_configs = [
     PrioritizerQueueConfig(
-        1, "https://sqs.us-east-2.amazonaws.com/061767223458/prioritizerQueue1", 80
+        1, "https://sqs.us-east-2.amazonaws.com/YOUR_URL_HERE", 80
     ),
     PrioritizerQueueConfig(
-        2, "https://sqs.us-east-2.amazonaws.com/061767223458/prioritizerQueue2", 20
+        2, "https://sqs.us-east-2.amazonaws.com/YOUR_URL_HERE", 20
     ),
 ]
 worker_queue_configs = [
-    QueueConfig(1, "https://sqs.us-east-2.amazonaws.com/061767223458/workerQueue1"),
-    QueueConfig(2, "https://sqs.us-east-2.amazonaws.com/061767223458/workerQueue2"),
+    QueueConfig(1, "https://sqs.us-east-2.amazonaws.com/YOUR_URL_HERE"),
+    QueueConfig(2, "https://sqs.us-east-2.amazonaws.com/YOUR_URL_HERE"),
 ]
-aws_region = "us-east-2"
-url_frontier_url = "https://sqs.us-east-2.amazonaws.com/061767223458/URLFrontierQueue"
+aws_region = "YOUR_REGION_HERE"
+url_frontier_url = "https://sqs.us-east-2.amazonaws.com/YOUR_URL_HERE"
 page_rank_api_url = "https://openpagerank.com/api/v1.0/getPageRank"
-page_rank_api_key = "8o4scsssoc44gokcwkk0cw8gg8g0kcwoowwscoso"
+page_rank_api_key = "YOUR_API_KEY_HERE"
 page_rank_request_headers = {"API-OPR": page_rank_api_key}
 redis_port = 6379
 redis_host = "localhost"
